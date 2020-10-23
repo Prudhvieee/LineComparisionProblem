ac7242819823113591124e020e7f88900f97946c
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-10-23 07:47:10 +0530

    [Prudhvi] Add . Adding uc-1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
 namespace LineComparisionProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //reading the values from the user.
+            Console.WriteLine("Enter the Value of x1 of first point : ");
+            float x1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of y1 of first point : ");
+            float y1 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of x2 of Second point : ");
+            float x2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of y2 of second point : ");
+            float y2 = Single.Parse(Console.ReadLine());
+            //calculating Line Length
+            double lineLength = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine($"Length of the line is {lineLength}");
         }
     }
 }
